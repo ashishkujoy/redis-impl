78273dd5ec55e32ea61f8bffb8a11f7cb290cded
Ashish <ashishkujoy@gmail.com>
2025-10-06 22:10:42 +0530

    Responds the client with pong

--- app/main.go
+++ app/main.go
@@ -21,9 +21,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer func(conn net.Conn) {
+		_ = conn.Close()
+	}(conn)
+	_, _ = conn.Write([]byte("PONG"))
 }
